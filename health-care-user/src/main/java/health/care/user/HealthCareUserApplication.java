23f8ba978c6160ca1721dc045c825057c1ec047c
ksreeram09 <sreeram.krv9@gmail.com>
2023-08-04 20:06:55 +0530

    health care user micro service project added

---
+++ health-care-user/src/main/java/health/care/user/HealthCareUserApplication.java
@@ -0,0 +1,13 @@
+package health.care.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareUserApplication.class, args);
+	}
+
+}
---
+++ health-care-user/src/test/java/health/care/user/HealthCareUserApplicationTests.java
@@ -0,0 +1,13 @@
+package health.care.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
